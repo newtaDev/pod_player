7b2c83b31613ddc60af1aa70b3af145d66a9c811
newtaDev <newta.dev@gmail.com>
2022-04-20 19:16:58 +0530

    pod_video_player_config renamed to pod_player_config

--- lib/pod_player.dart
+++ lib/pod_player.dart
@@ -6,7 +6,7 @@ export 'src/controllers/pod_player_controller.dart';
 export 'src/models/overlay_options.dart';
 export 'src/models/play_video_from.dart';
 export 'src/models/pod_progress_bar_config.dart';
-export 'src/models/pod_video_player_config.dart';
+export 'src/models/pod_player_config.dart';
 export 'src/models/vimeo_models.dart';
 export 'src/pod_player.dart';
 export 'src/utils/enums.dart';
