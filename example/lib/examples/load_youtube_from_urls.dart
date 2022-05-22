e0e4f92f9104f9123bb95a934ec477748e24e905
newtaDev <newta.dev@gmail.com>
2022-05-22 19:13:31 +0530

    file name changed

---
+++ example/lib/examples/load_youtube_from_urls.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:pod_player/pod_player.dart';
+
+void main(List<String> args) {
+  runApp(const YoutubeApp());
+}
+
+class YoutubeApp extends StatelessWidget {
+  const YoutubeApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar:
+            AppBar(title: const Text('Load youtube video from quality urls')),
+        body: const YoutubeVideoViewer(),
+      ),
+    );
+  }
+}
+
+class YoutubeVideoViewer extends StatefulWidget {
+  const YoutubeVideoViewer({Key? key}) : super(key: key);
+
+  @override
+  State<YoutubeVideoViewer> createState() => _YoutubeVideoViewerState();
+}
+
+class _YoutubeVideoViewerState extends State<YoutubeVideoViewer> {
+  late final PodPlayerController controller;
+  bool isLoading = true;
+  @override
+  void initState() {
+    loadVideo();
+    super.initState();
+  }
+
+  void loadVideo() async {
+    final _urls = await PodPlayerController.getYoutubeUrls(
+      'https://youtu.be/A3ltMaM6noM',
+    );
+    setState(() => isLoading = false);
+    controller = PodPlayerController(
+      playVideoFrom: PlayVideoFrom.networkQualityUrls(videoUrls: _urls!),
+      podPlayerConfig: const PodPlayerConfig(
+        initialVideoQuality: 360,
+      ),
+    )..initialise();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : Center(child: PodVideoPlayer(controller: controller));
+  }
+}
