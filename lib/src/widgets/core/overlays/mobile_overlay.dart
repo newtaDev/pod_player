884691dbf2fe61b3630ad92f6aa450cdf1522e24
Deary Ismail <117909226+deary-ismail@users.noreply.github.com>
2024-01-02 13:22:02 +0300

    Update mobile_overlay.dart
    I removed the Ingnore widget of videoTitle

--- lib/src/widgets/core/overlays/mobile_overlay.dart
+++ lib/src/widgets/core/overlays/mobile_overlay.dart
@@ -57,9 +57,7 @@ class _MobileOverlay extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Expanded(
-                child: IgnorePointer(
-                  child: podCtr.videoTitle ?? const SizedBox(),
-                ),
+                child: child: podCtr.videoTitle ?? const SizedBox(),
               ),
               MaterialIconButton(
                 toolTipMesg: podCtr.podPlayerLabels.settings,
